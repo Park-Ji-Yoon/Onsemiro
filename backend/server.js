623954029854c007046e899e6f208947aff5e69c
Jiyoon Park <55547842+Park-Ji-Yoon@users.noreply.github.com>
2021-06-22 11:59:01 +0900

    테스트 계정으로 로그인 기능 추가

--- backend/server.js
+++ backend/server.js
@@ -148,6 +148,52 @@ app.post('/login_process', function(req, res) {
   res.redirect('/login_success?' + query);
 });
 
+app.post('/test_login', function(req, res) {
+  //로그인을할때 아이디와 비밀번호를 받는다
+  let testMail = "mirim3210@gmail.com";
+  let testPw = "123456"
+  User.findOne({ email: testMail }, (err, user) => {
+    if (err) {
+      return res.json({
+        loginSuccess: false,
+        message: "존재하지 않는 아이디입니다.",
+      });
+    }
+    user
+      .comparePassword(testPw)
+      .then((isMatch) => {
+        if (!isMatch) {
+          return res.json({
+            loginSuccess: false,
+            message: "비밀번호가 일치하지 않습니다",
+          });
+        }
+      //비밀번호가 일치하면 토큰을 생성한다
+      //jwt 토큰 생성하는 메소드 작성
+      user
+        .generateToken()
+        .then((user) => {
+          res
+            .cookie("x_auth", user.token)
+            .status(200)
+            .json({ loginSuccess: true, userId: user._id });
+        })
+        .catch((err) => {
+          // res.status(400).send(err);
+        });
+    })
+    .catch((err) => res.json({ loginSuccess: false, err }));
+
+    req.session.user = {
+			email: testMail,
+		};
+  });
+  const query = querystring.stringify({
+    "email": testMail
+  });
+  res.redirect('/login_success?' + query);
+});
+
 app.get('/login_success', function(req, res){
   var queryData = url.parse(req.url, true).query;
   // cookie생성
